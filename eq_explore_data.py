c819716c8ad77f2082baf7bf9e8902fa559fce0a
Stefanie Seaney <stefanie.seaney@gmail.com>
2021-02-18 13:35:17 -0600

    Variable name reset.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -2,8 +2,8 @@ import json
 
 # Explore the structure of the data.
 filename = "data/eq_data_1_day_m1.json"
-with open(filename) as file:
-    all_eq_data = json.load(file)
+with open(filename) as f:
+    all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data["features"]
 
